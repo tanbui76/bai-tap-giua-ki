c56005ca33869e977b199ea7f9d57d2c7cdcf021
Bùi Nguyễn Nhật Tân <84611805+tanbui76@users.noreply.github.com>
2022-11-29 00:46:36 +0700

    tan

---
+++ src/main/java/QuanliCLB/dbconnection/dbConnection.java
@@ -0,0 +1,21 @@
+package QuanliCLB.dbconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConnection {
+	private static String url = "jbdc:mysql://sql6.freesqldatabase.com:3306/sql6580911";       
+    private static String username = "sql6580911";   
+    private static String password = "1pxhNkIdab";
+    private static Connection connection = null;
+    
+    public static Connection getConnection() throws ClassNotFoundException, SQLException{
+        if(connection == null){
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection=DriverManager.getConnection(url,username,password);
+            System.out.print("connected");
+        }
+        return connection;
+    }
+}
