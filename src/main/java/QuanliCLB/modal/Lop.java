085e4683639071e991bdabea9a950f624bbaca8b
Bùi Nguyễn Nhật Tân <84611805+tanbui76@users.noreply.github.com>
2022-11-30 08:01:34 +0700

    tan

---
+++ src/main/java/QuanliCLB/modal/Lop.java
@@ -0,0 +1,33 @@
+package QuanliCLB.modal;
+
+public class Lop {
+	private int idLop;
+	private String tenLop;
+	
+	public Lop() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Lop(int idLop, String tenLop) {
+		this.idLop = idLop;
+		this.tenLop = tenLop;
+	}
+
+	public int getIdLop() {
+		return idLop;
+	}
+
+	public void setIdLop(int idLop) {
+		this.idLop = idLop;
+	}
+
+	public String getTenLop() {
+		return tenLop;
+	}
+
+	public void setTenLop(String tenLop) {
+		this.tenLop = tenLop;
+	}
+	
+	
+}
---
+++ src/main/java/QuanliCLB/modal/MonHoc.java
@@ -0,0 +1,43 @@
+package QuanliCLB.modal;
+
+public class MonHoc {
+	private int idMonHoc;
+	private String tenMonhoc;
+	private int tongSoTiet;
+	
+	public MonHoc() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public MonHoc(int idMonHoc, String tenMonhoc, int tongSoTiet) {
+		this.idMonHoc = idMonHoc;
+		this.tenMonhoc = tenMonhoc;
+		this.tongSoTiet = tongSoTiet;
+	}
+
+	public int getIdMonHoc() {
+		return idMonHoc;
+	}
+
+	public void setIdMonHoc(int idMonHoc) {
+		this.idMonHoc = idMonHoc;
+	}
+
+	public String getTenMonhoc() {
+		return tenMonhoc;
+	}
+
+	public void setTenMonhoc(String tenMonhoc) {
+		this.tenMonhoc = tenMonhoc;
+	}
+
+	public int getTongSoTiet() {
+		return tongSoTiet;
+	}
+
+	public void setTongSoTiet(int tongSoTiet) {
+		this.tongSoTiet = tongSoTiet;
+	}
+	
+	
+}
