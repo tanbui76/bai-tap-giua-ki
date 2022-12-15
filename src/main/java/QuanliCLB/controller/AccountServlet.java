86ba6e2b62d1cd508e552eec6a23263d29a7e00e
Thái Thị Thanh Ngân <94752353+ThanhNgan110@users.noreply.github.com>
2022-12-15 14:35:42 +0700

    ngan

--- src/main/java/QuanliCLB/controller/AccountServlet.java
+++ src/main/java/QuanliCLB/controller/AccountServlet.java
@@ -14,10 +14,24 @@ import javax.servlet.http.HttpSession;
 import QuanliCLB.dao.AccountDAO;
 import QuanliCLB.model.TaiKhoan;
 
+/**
+ * Servlet implementation class AccountServlet
+ */
 @WebServlet("/AccountServlet")
 public class AccountServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AccountServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		AccountDAO aDao = new AccountDAO();
 		HttpSession session = request.getSession();
@@ -31,6 +45,9 @@ public class AccountServlet extends HttpServlet {
 		}
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
