b3916de7fa9d3f51b0c7581825fadd4511c8169a
Thái Thị Thanh Ngân <94752353+ThanhNgan110@users.noreply.github.com>
2022-12-22 14:50:42 +0700

    ngan Merge branch 'master' of https://github.com/tanbui76/bai-tap-giua-ki.git

---
+++ src/main/java/QuanliCLB/controller/StudentScoreServlet.java
@@ -0,0 +1,36 @@
+package QuanliCLB.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StudentScoreServlet
+ */
+@WebServlet("/StudentScoreServlet")
+public class StudentScoreServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StudentScoreServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
